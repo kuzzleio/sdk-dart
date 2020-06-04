786e4c3dc89208bc879256f7fe5bcd9a41a2b511
Yoann Abbes <yoann1.abbes@epitech.eu>
2020-06-04 04:27:17 +0200

    revert test change

--- test/authentication_test.dart
+++ test/authentication_test.dart
@@ -27,7 +27,7 @@ void main() {
         final saveduser = await kuzzle.security
             .createUser({'local': credentials}, user.content);
         expect(saveduser.content['name'], user.content['name']);
-        user.content = saveduser;
+        user = saveduser;
       });
 
       test('login', () async {
