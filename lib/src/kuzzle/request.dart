811360a4cd60971840b9097fe5034825a08735f7
Yoann Abbes <yoann1.abbes@epitech.eu>
2020-07-19 06:10:35 +0200

    security:createApiKey and security:deleteApiKey

--- lib/src/kuzzle/request.dart
+++ lib/src/kuzzle/request.dart
@@ -30,6 +30,7 @@ class KuzzleRequest {
       this.scope,
       this.state,
       this.source,
+      this.userId,
       this.users,
       this.includeKuzzleMeta}) {
     requestId ??= _uuid.v4();
@@ -61,6 +62,7 @@ class KuzzleRequest {
     reset = request.reset;
     scope = request.scope;
     state = request.state;
+    userId = request.userId;
     users = request.users;
     source = request.source;
     includeKuzzleMeta = request.includeKuzzleMeta;
@@ -97,6 +99,7 @@ class KuzzleRequest {
     reset = data['reset'] as bool;
     scope = data['scope'] as String;
     state = data['state'] as String;
+    userId = data['userId'] as String;
     users = data['users'] as String;
     source = data['source'] as bool;
     includeKuzzleMeta = data['includeKuzzleMeta'] as bool;
@@ -182,6 +185,9 @@ class KuzzleRequest {
     if (state != null) {
       map['state'] = state;
     }
+    if (userId != null) {
+      map['userId'] = userId;
+    }
     if (users != null) {
       map['users'] = users;
     }
@@ -225,6 +231,7 @@ class KuzzleRequest {
   bool source;
   String scope;
   String state;
+  String userId;
   String users;
   bool includeKuzzleMeta;
 }
