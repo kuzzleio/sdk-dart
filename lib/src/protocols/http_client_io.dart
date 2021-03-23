c9e26802d843fe60475d52d47acc62d7cecb7e3a
jenow <jenova.ke20@gmail.com>
2021-03-23 16:38:38 +0100

    remove print

--- lib/src/protocols/http_client_io.dart
+++ lib/src/protocols/http_client_io.dart
@@ -4,7 +4,6 @@ import 'package:http/http.dart';
 import 'package:http/io_client.dart';
 
 BaseClient createHttpClient({bool acceptBadCertificate = false}) {
-  print('io');
   final client = HttpClient()
     ..badCertificateCallback = ((cert, host, port) => acceptBadCertificate);
   return IOClient(client);
