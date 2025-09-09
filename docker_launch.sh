2d0a6a937abea1145b84c5335ee3519e58ff9f0c
EnzoViry1 <eviry@kuzzle.io>
2025-09-09 10:23:09 +0200

    fix(ci): automatic publish pub.dev

--- docker_launch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sysctl -w vm.max_map_count=262144
-docker compose up -d
-until curl "http://localhost:7512/?pretty"; do sleep 10; done
-./adminauth.sh
-dart pub get
---
+++ scripts/publish.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+# Extract version line from pubspec.yaml
+VERSION_LINE=$(grep '^version: ' pubspec.yaml)
+VERSION=$(echo "$VERSION_LINE" | sed 's/version: //')
+
+# Split version into main and build parts
+MAIN_VERSION=$(echo "$VERSION" | cut -d'+' -f1)
+BUILD_VERSION=$(echo "$VERSION" | cut -s -d'+' -f2)
+
+# Split main version into major, minor, patch
+IFS='.' read -r MAJOR MINOR PATCH <<< "$MAIN_VERSION"
+
+# Increment patch
+PATCH=$((PATCH + 1))
+
+# Reconstruct new version
+NEW_VERSION="${MAJOR}.${MINOR}.${PATCH}"
+if [ -n "$BUILD_VERSION" ]; then
+  NEW_VERSION="${NEW_VERSION}+${BUILD_VERSION}"
+fi
+
+# Update pubspec.yaml
+sed -i "s/^version: .*/version: ${NEW_VERSION}/" pubspec.yaml
+
+# Git tag and push
+git tag -a "$NEW_VERSION" -m "Release $NEW_VERSION"
+git push origin "$NEW_VERSION"
